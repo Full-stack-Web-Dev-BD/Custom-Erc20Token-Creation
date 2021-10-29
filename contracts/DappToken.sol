b5075f2c063079368708d14a196e38b0490d0e40
unknown <Alamin>
2021-10-29 20:28:04 +0600

    tb

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -6,8 +6,15 @@ contract DappToken {
     uint256 public totalSupply;
     string public standerd = "DApp Token v1.0";
     mapping(address => uint256) public balanceOf;
-
+    // allowance
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    // transfer event
 
     constructor(
         uint256 _initialSupply,
@@ -30,4 +37,16 @@ contract DappToken {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
+
+    // _spender is  upcomming owner by the transection
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // allowance
+        // approve event
+        emit Approve(msg.sender, _spender, _value);
+        return true;
+    }
+    // transferFrom
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,7 +1,6 @@
 var DappToken = artifacts.require('./DappToken.sol')
 
 contract("DappToken", async (accounts) => {
-
     it('Initializes the contract with the correct values', async () => {
         let dappTokenInstance = await DappToken.deployed()
         let tokenName = await dappTokenInstance.name()
@@ -25,11 +24,24 @@ contract("DappToken", async (accounts) => {
         let log = (await dappTokenInstance.transfer(accounts[1], 250000, { from: accounts[0] })).receipt.logs
         assert.equal(log.length, 1, "triggers one event ");
         assert.equal(log[0].event, "Transfer", 'should be "Transfer" event')
-        assert.equal(log[0].args._from, accounts[0], 'logs the acount  the tokens are  fransferred from ')
-        assert.equal(log[0].args._to, accounts[1], 'logs the acount  the tokens are  fransferred to')
+        assert.equal(log[0].args._from, accounts[0], 'logs the account  the tokens are  fransferred from ')
+        assert.equal(log[0].args._to, accounts[1], 'logs the account  the tokens are  fransferred to')
         let balance = (await dappTokenInstance.balanceOf(accounts[1])).toNumber()
         let senderBalance = (await dappTokenInstance.balanceOf(accounts[0])).toNumber()
         assert.equal(balance, 250000, "adds the amount to the receiving account")
         assert.equal(senderBalance, 750000, "deducts amount to the sending account")
+    })
+    it("approve tokens for delegated transfer ", async () => {
+        let dappTokenInstance = await DappToken.deployed()
+        let approveResult = await dappTokenInstance.approve.call(accounts[0], 100)
+        assert.equal(approveResult, true, "it returns true")
+        let approveEvent = (await dappTokenInstance.approve(accounts[1], 100)).receipt.logs
+        assert.equal(approveEvent.length, 1, "triggers one event ");
+        assert.equal(approveEvent[0].event, "Approve", 'should be "Approve " event')
+        assert.equal(approveEvent[0].args._owner, accounts[0], 'logs the account  the tokens are  owner ')
+        assert.equal(approveEvent[0].args._spender, accounts[1], 'logs the account  the tokens are  spender')
+        assert.equal(approveEvent[0].args._value, 100, 'logs the transfer amount')
+
+
     })
 })
