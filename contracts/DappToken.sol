286c747de76540c125184115ea5d1f4f152ae655
unknown <Alamin>
2021-10-29 19:52:19 +0600

    done transfer token  with test

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,33 @@
 pragma solidity >=0.4.0 <0.9.0;
 
 contract DappToken {
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
+    string public name;
+    string public symbol;
     uint256 public totalSupply;
+    string public standerd = "DApp Token v1.0";
+    mapping(address => uint256) public balanceOf;
 
-    constructor() public {
-        totalSupply = 1000000;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    constructor(
+        uint256 _initialSupply,
+        string memory _name,
+        string memory _symbol
+    ) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,11 +1,35 @@
 var DappToken = artifacts.require('./DappToken.sol')
 
 contract("DappToken", async (accounts) => {
+
+    it('Initializes the contract with the correct values', async () => {
+        let dappTokenInstance = await DappToken.deployed()
+        let tokenName = await dappTokenInstance.name()
+        let tokenSymbol = await dappTokenInstance.symbol()
+        let standerd = await dappTokenInstance.standerd()
+        assert.equal(tokenName, "BDTC", "Set the token name !")
+        assert.equal(tokenSymbol, "BDTCSymbol", "Set the token symbol !");
+        assert.equal(standerd, "DApp Token v1.0", "Has the correct version !");
+    })
     it("Sets the total suppley upon deployment ", async () => {
         let dappTokenInstance = await DappToken.deployed()
-        let totalSupplyBN = await dappTokenInstance.totalSupply()
-        let totalSupply = totalSupplyBN.toNumber()
-        return assert.equal(totalSupply, 1000000, 'Sets the total supply to 1M')
+        let totalSupply = (await dappTokenInstance.totalSupply()).toNumber()
+        let adminBalance = (await dappTokenInstance.balanceOf(accounts[0])).toNumber()
+        assert.equal(totalSupply, 1000000, 'Sets the total supply to 1M')
+        assert.equal(adminBalance, 1000000, 'It allocated as initial supply to the admin account')
     })
-
+    it("transfer token ownership", async () => {
+        let dappTokenInstance = await DappToken.deployed()
+        let success = (await dappTokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] }))
+        assert.equal(success, true, "it should return true");
+        let log = (await dappTokenInstance.transfer(accounts[1], 250000, { from: accounts[0] })).receipt.logs
+        assert.equal(log.length, 1, "triggers one event ");
+        assert.equal(log[0].event, "Transfer", 'should be "Transfer" event')
+        assert.equal(log[0].args._from, accounts[0], 'logs the acount  the tokens are  fransferred from ')
+        assert.equal(log[0].args._to, accounts[1], 'logs the acount  the tokens are  fransferred to')
+        let balance = (await dappTokenInstance.balanceOf(accounts[1])).toNumber()
+        let senderBalance = (await dappTokenInstance.balanceOf(accounts[0])).toNumber()
+        assert.equal(balance, 250000, "adds the amount to the receiving account")
+        assert.equal(senderBalance, 750000, "deducts amount to the sending account")
+    })
 })
