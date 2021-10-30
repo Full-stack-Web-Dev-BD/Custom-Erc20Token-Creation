8969eb15630cb9c0562722b551eea70c3a41e21b
unknown <Alamin>
2021-10-30 23:16:27 +0600

    Part 6- Done buye token functionalities

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -3,15 +3,32 @@ import "./DappToken.sol";
 
 contract DappTokenSale {
     address admin;
-    DappToken public tokenContract;
+    DappToken public dappToken;
     uint256 public tokenPrice;
+    uint256 public tokenSold;
+
+    // events
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
         // Assign admin
         admin = msg.sender;
-        tokenContract = _tokenContract;
+        dappToken = _tokenContract;
         tokenPrice = _tokenPrice;
         // Token Contract
         // Token Price
+    }
+
+    // multiply
+    function multiply(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfToken) public payable {
+        require(msg.value == multiply(_numberOfToken, tokenPrice));
+        require(dappToken.balanceOf(address(this)) >= _numberOfToken);
+        require(dappToken.transfer(msg.sender, _numberOfToken)); // this is the  actual  buy functionality
+        tokenSold += _numberOfToken;
+        emit Sell(msg.sender, _numberOfToken);
     }
 }
---
+++ test/DappTokenSale.js
@@ -0,0 +1,44 @@
+const DappToken = artifacts.require('DappToken')
+const DappTokenSale = artifacts.require('DappTokenSale')
+
+
+contract("DappTokenSale", async (accounts) => {
+    var dtsc;//tokdn sale contract
+    var dtc;//dapp token contract
+    var myTokenPrice = 1000000000000000000;// in wai 
+    var admin = accounts[0]
+    var buyer = accounts[1]
+    var tokenAvailable = 750000
+
+    it("initializez the contract with the correct values", async () => {
+        dtsc = await DappTokenSale.deployed()
+        tsAddress = dtsc.address
+        assert.notEqual(tsAddress, 0x0, "has contract address ")
+
+        let tokenContract = await dtsc.tokenContract
+        assert.notEqual(tokenContract, 0x0, "has tokenContract address ")
+
+        let tokenPrice = await dtsc.tokenPrice()
+        assert.equal(tokenPrice, myTokenPrice, "token price  is correct")
+    })
+
+    it('facilitates token buying ', async () => {
+        var numberOfToken = 10
+        var value = numberOfToken * myTokenPrice
+        dtsc = await DappTokenSale.deployed()
+        dtc = await DappToken.deployed()
+        await dtc.transfer(dtsc.address, tokenAvailable, { from: admin })
+        var receipt = (await dtsc.buyTokens(numberOfToken, { from: buyer, value: value })).receipt.logs
+        assert.equal(receipt.length, 1, "triggers one event")
+        assert.equal(receipt[0].event, "Sell", "should be call 'Sell' event ")
+        assert.equal(receipt[0].args._buyer, buyer, "log the account purchased token")
+        assert.equal((await receipt[0].args._amount).toNumber(), numberOfToken, "log the amount of token sold ")
+        var soldToken = await dtsc.tokenSold()
+        assert.equal(soldToken, numberOfToken, "increment the number of tokens sold")
+
+        let buyerBalance = await dtc.balanceOf(buyer)
+        assert.equal(buyerBalance.toNumber(), numberOfToken, "should be match with purchased amount")
+
+    })
+
+})
